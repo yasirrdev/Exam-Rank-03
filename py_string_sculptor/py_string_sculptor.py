d9329d8b8e708fad6d902109bee9e9570ac6e34f
Yasir <157069639+yasirrdev@users.noreply.github.com>
2026-04-27 16:24:33 +0200

    Update py_string_sculptor.py

--- py_string_sculptor/py_string_sculptor.py
+++ py_string_sculptor/py_string_sculptor.py
@@ -13,3 +13,14 @@ def sculptor(text: str) -> str:
         else:
             s += i
     return s
+
+
+if __name__ == "__main__":
+    print((sculptor("Hello world")))    # → "hElLo WoRlD"
+    print(sculptor("Hello, world!"))  # → "hElLo, WoRlD!"
+    print(sculptor("123abcDEF"))      # → "123aBcDeF"
+    print(sculptor("a-bC-dEf-ghIj"))  # → "a-Bc-DeF-gHiJ"
+    print(sculptor(""))               # → ""
+    print(sculptor("12345"))          # → "12345"
+    print(sculptor("A"))              # → "a"
+    print(sculptor("ab"))             # → "aB"
